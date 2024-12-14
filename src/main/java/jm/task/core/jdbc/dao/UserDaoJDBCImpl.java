55a8867f3741923cc38f78e11353a6ef05043630
Azamat244 <152506960+Azamat244@users.noreply.github.com>
2024-12-14 21:07:45 +0500

    решение задачи 1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,103 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
+    Connection connection = Util.getConnection();
+    Logger logger = Logger.getLogger(UserDaoJDBCImpl.class.getName());
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY," +
+                "name VARCHAR(100), " +
+                "last_name VARCHAR(100), " +
+                "age INT" +
+                ")";
+
+       try (Statement statement = connection.createStatement()) {
+           statement.execute(sql);
+           logger.info("Таблица создана");
+       }
+       catch (SQLException e) {
+            throw new RuntimeException("Ошибка создания таблицы",e);
+        }
 
     }
 
     public void dropUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(sql);
+            logger.info("Таблица удалена");
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка удаления таблицы",e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setInt(3, age);
+            preparedStatement.executeUpdate();
+            logger.info("User с именем — " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка добавления пользователя", e);
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            logger.info("Пользователь с id = " + id + " удален");
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка удаления пользователя по id", e);
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM users";
+        List<User> users = new ArrayList<>();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte("age"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка получения данных из таблицы",e);
+        }
+        System.out.println(users);
+        return users;
     }
 
     public void cleanUsersTable() {
+        String sql = "DELETE FROM users";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка удаления данных из таблицы",e);
+        }
 
     }
 }
